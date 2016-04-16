441e1149f11fd55fe4b089157163dd84319f425f
yurabysaha <yurabysaha@gmail.com>
2016-04-16 18:38:50 +0300

    s3 storage 4

--- app/models/factory.rb
+++ app/models/factory.rb
@@ -3,8 +3,13 @@ class Factory < ActiveRecord::Base
   has_many :items
 
   has_attached_file :avatar,
-                    :storage => :s3,
-                    :s3_credentials => {:bucket => "stalkerzona", :access_key_id => "AKIAJXEM5TEGB3D4AJ6Q", :secret_access_key => "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"}
+        :storage => :s3,
+                   :bucket => 'stalkerzona',
+                   :s3_credentials => {
+                                :access_key_id => 'AKIAJXEM5TEGB3D4AJ6Q',
+                                :secret_access_key => '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'},
+                 :s3_protocol    => "https",
+                 :s3_host_name   => "s3-eu-central-1.amazonaws.com"
 
   validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
